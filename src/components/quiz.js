a942d7eeba17edc769ea7f1ab56571e30a2c453d
Prasanga Dhakal <prasanga.dhakal@gmail.com>
2019-03-13 15:48:06 -0230

    answer options look pretty

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -32,8 +32,16 @@ export class Quiz extends React.Component {
         if (this.state.q === 0) {
             return (
                 <div>
-                    <h1>Question {1}</h1>
-                    {questions[0]}
+                    <h3><span className="badge badge-primary">Question {1}</span></h3>
+                    <h3>{questions[0]}</h3>
+                    <br />
+                    <div className="row">
+                        <div className="col badge badge-info p-3 m-2">Jakarta</div>
+                        <div className="col badge badge-info p-3 m-2">Kuala Lumpur</div>
+                        <div className="w-100"></div>
+                        <div className="col badge badge-info p-3 m-2">Kathmandu</div>
+                        <div className="col badge badge-info p-3 m-2">Ulan Bator</div>
+                    </div>
                     <br />
                     <button className="btn btn-secondary" onClick={this.onclick.bind(this, 'add')}>Next question</button>
                 </div>
@@ -44,8 +52,8 @@ export class Quiz extends React.Component {
             if (this.state.q === i) {
                 return (
                     <div>
-                        <h1>Question {i + 1}</h1>
-                        {questions[i]}
+                        <h3><span className="badge badge-primary">Question {i + 1}</span></h3>
+                        <h3>{questions[i]}</h3>
                         <br />
                         <div className="btn-group" >
                             <button className="btn btn-secondary" onClick={this.onclick.bind(this, 'add')}>Next question</button>
