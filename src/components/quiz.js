e012c4760b7e33334631cdd37d903485f290edbf
Prasanga Dhakal <prasanga.dhakal@gmail.com>
2019-03-13 16:19:22 -0230

    made 2d array to store answers to each questions and made a separate scoreboard component

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {Scoreboard} from '../components/scoreboard';
 
 let questions = [
-    "What is the capital of Nepal?",
-    "Who was the first prime minister of Nepal?",
-    "How many of the world's tallest mountains are situated in Nepal?",
-    "Who was the first Nepalese person to climb Mt. Everest?",
-    "What is the national flower of Nepal?",
-    "What is the second largest religion in Nepal?"
+    ["What is the capital of Nepal?", "Jakarta", "Kuala Lumpur", "Kathmandu", "Ulan Bator"],
+    ["Who was the first prime minister of Nepal?", "Bhakti Thapa", "Jung Bahadur Rana", "Bishweswor Prasad Koirala", "Bhimsen Thapa"],
+    ["How many of the world's tallest mountains are situated in Nepal?", "3", "6", "8", "9"],
+    ["Who was the first Nepalese person to climb Mt. Everest?", "Tenzing Norgay Sherpa", "Ani Choing Dolma", "Ramkrishna Dhakal", "Pemba Sherpa"],
+    ["What is the national flower of Nepal?", "Lotus", "Rhododendron", "Amaryllis", "Begonia"],
+    ["What is the second largest religion in Nepal?", "Islam", "Hinduism", "Christianity", "Buddhism"]
 ]
 
 let correctAnswers = [
@@ -33,14 +34,14 @@ export class Quiz extends React.Component {
             return (
                 <div>
                     <h3><span className="badge badge-primary">Question {1}</span></h3>
-                    <h3>{questions[0]}</h3>
+                    <h3>{questions[0][0]}</h3>
                     <br />
                     <div className="row">
-                        <div className="col badge badge-info p-3 m-2">Jakarta</div>
-                        <div className="col badge badge-info p-3 m-2">Kuala Lumpur</div>
+                        <div className="col badge badge-info p-3 m-2">{questions[0][1]}</div>
+                        <div className="col badge badge-info p-3 m-2">{questions[0][2]}</div>
                         <div className="w-100"></div>
-                        <div className="col badge badge-info p-3 m-2">Kathmandu</div>
-                        <div className="col badge badge-info p-3 m-2">Ulan Bator</div>
+                        <div className="col badge badge-info p-3 m-2">{questions[0][3]}</div>
+                        <div className="col badge badge-info p-3 m-2">{questions[0][4]}</div>
                     </div>
                     <br />
                     <button className="btn btn-secondary" onClick={this.onclick.bind(this, 'add')}>Next question</button>
@@ -53,7 +54,15 @@ export class Quiz extends React.Component {
                 return (
                     <div>
                         <h3><span className="badge badge-primary">Question {i + 1}</span></h3>
-                        <h3>{questions[i]}</h3>
+                        <h3>{questions[i][0]}</h3>
+                        <br />
+                        <div className="row">
+                            <div className="col badge badge-info p-3 m-2">{questions[i][1]}</div>
+                            <div className="col badge badge-info p-3 m-2">{questions[i][2]}</div>
+                            <div className="w-100"></div>
+                            <div className="col badge badge-info p-3 m-2">{questions[i][3]}</div>
+                            <div className="col badge badge-info p-3 m-2">{questions[i][4]}</div>
+                        </div>
                         <br />
                         <div className="btn-group" >
                             <button className="btn btn-secondary" onClick={this.onclick.bind(this, 'add')}>Next question</button>
@@ -65,9 +74,10 @@ export class Quiz extends React.Component {
                 );
             }
         }
+
         return (
             <div>
-                Score
+                <Scoreboard />
             </div>
         );
     }
---
+++ src/components/scoreboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Scoreboard extends React.Component {
+    render(){
+        return(
+            <div>
+                This is Score
+            </div>
+        );
+    }
+}
